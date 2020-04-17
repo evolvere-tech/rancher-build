bc8fd04bcb9867ef9022916a91c058575d87b459
Dan Carter <37019748+dan-evolvere@users.noreply.github.com>
2020-04-17 14:19:08 +0100

    Update azure-setup.sh

--- nginx/azure-setup.sh
+++ nginx/azure-setup.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 sudo bash <(curl -s http://example.com/update.sh)
 sudo bash <(curl -s http://example.com/install-trend.sh)
-sudo bash <(curl -s http://example.com/cloud-init.sh)
 sudo bash <(curl -s http://example.com/cleanup.sh)
