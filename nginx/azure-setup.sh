addc71ab30ffa40e0d03a972c8f710bd812e0f82
Dan Carter <37019748+dan-evolvere@users.noreply.github.com>
2020-04-17 14:15:18 +0100

    remote installer

---
+++ nginx/azure-setup.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+sudo bash <(curl -s http://example.com/update.sh)
+sudo bash <(curl -s http://example.com/install-trend.sh)
+sudo bash <(curl -s http://example.com/cloud-init.sh)
+sudo bash <(curl -s http://example.com/cleanup.sh)
